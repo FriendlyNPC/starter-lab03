c5b68110b64069abc0cdbd74118b2567f11bdeb8
FriendlyNPC <Devanyim@gmail.com>
2015-09-24 16:47:37 -0700

    Revert "'sleep' link fixed"
    This reverts commit c315658473015c47f2329fab36d3d86d1bc0e8fa.

--- application/config/routes.php
+++ application/config/routes.php
@@ -41,8 +41,6 @@
 $route['default_controller'] = "welcome";
 $route['404_override'] = '';
 
-$route['sleep'] = 'first/sleep';
-
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
