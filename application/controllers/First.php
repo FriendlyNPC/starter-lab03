4627d66212398ccc1f7dda7e4d3ab5d8e86a02ad
FriendlyNPC <Devanyim@gmail.com>
2015-09-24 16:23:48 -0700

    created first page

---
+++ application/controllers/First.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Our first. Show a table of all the author pictures. Clicking on one should show their quote.
+ * Our quotes model has been autoloaded, because we use it everywhere.
+ * 
+ * controllers/First.php
+ *
+ * ------------------------------------------------------------------------
+ */
+class First extends Application {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    //-------------------------------------------------------------
+    //  The normal pages
+    //-------------------------------------------------------------
+
+    function index() {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        
+        $record = $this->quotes->first();
+        
+        $this->data['mug'] = $record['mug'];
+        $this->data['what'] = $record['what'];
+        $this->data['who'] = $record['who'];
+        
+        $this->render();
+    }
+
+}
+
+/* End of file First.php */
+/* Location: application/controllers/First.php */
