4c85b4d4154ff217811e9a8aa62a5d0edd179d78
FriendlyNPC <Devanyim@gmail.com>
2015-09-24 18:14:07 -0700

    fixed routing regex

--- application/config/routes.php
+++ application/config/routes.php
@@ -48,7 +48,7 @@ $route['show/(:num)'] = 'first/gimmie/$1';
 $route['dunno'] = 'guess'; 
 $route['([a-zA-Z]{4})/bingo'] = 'bingo';
 
-$route['(comp\d+)/(:any)'] = 'bingo/wisdom';
+$route['(comp\d+)/(.*)'] = 'bingo/wisdom';
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
